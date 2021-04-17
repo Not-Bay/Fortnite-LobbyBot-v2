1303049e90e9535eb11e0879727f29298d907786
gomashio <gomashio1596@gmail.com>
2021-04-17 18:55:07 +0900

    Fuck repl

--- modules/session_id.py
+++ modules/session_id.py
@@ -1,11 +1,11 @@
+import importlib
 from typing import Any
 
-from fortnitepy.http import EpicGames
-from fortnitepy.auth import *
+fortnitepy = importlib.import_module('fortnitepy')
 
 
-class SessionIDAuth(ExchangeCodeAuth):
-    def __init__(self, sid: StrOrMaybeCoro,
+class SessionIDAuth(fortnitepy.ExchangeCodeAuth):
+    def __init__(self, sid: fortnitepy.auth.StrOrMaybeCoro,
                  **kwargs: Any) -> None:
         super().__init__(sid, **kwargs)
 
@@ -14,7 +14,7 @@ class SessionIDAuth(ExchangeCodeAuth):
         return response.cookies['XSRF-TOKEN'].value
 
     async def set_sid(self, sid: str, priority: int = 0) -> str:
-        r = EpicGames('/id/api/set-sid')
+        r = fortnitepy.http.EpicGames('/id/api/set-sid')
         return await self.client.http.get(
             r,
             params={'sid': sid},
@@ -26,21 +26,21 @@ class SessionIDAuth(ExchangeCodeAuth):
 
         try:
             await self.set_sid(self.resolved_code)
-        except HTTPException as e:
+        except fortnitepy.HTTPException as e:
             m = 'errors.com.epicgames.accountportal.session_id_invalid'
             if e.message_code == m:
-                raise AuthException(
+                raise fortnitepy.AuthException(
                     'Invalid session id supplied',
                     e
                 ) from e
 
             raise
 
-        log.info('Fetching valid xsrf token.')
+        fortnitepy.auth.log.info('Fetching valid xsrf token.')
         token = await self.fetch_xsrf_token()
         await self.client.http.epicgames_reputation(token)
 
-        log.info('Fetching exchange code.')
+        fortnitepy.auth.log.info('Fetching exchange code.')
         data = await self.client.http.epicgames_get_exchange_data(token)
 
         data = await self.exchange_code_for_session(
